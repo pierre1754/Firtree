6d34cb373875404e6f254823c00358a8c0aee63f
pierre1754 <pierre1754@outlook.com>
2020-10-04 01:00:37 +0200

    in the way

--- tree.c
+++ tree.c
@@ -33,7 +33,7 @@ int heigh(int size)
     return result;
 }
 
-int nbSpacesMAX(int size)
+int nbSpaces_max(int size)
 {
     int result = (4 * size) - size;
 
@@ -52,25 +52,35 @@ void my_put_star(int nbr_star)
         my_putchar('*');
 }
 
-void triangle(int rank, int size)
+void triangle(int rank, int size, int *rank_sup)
 {
     int nbr_star = 1;
     int space = 4 + rank;
-    int i = nbSpacesMAX(size);
+    int i = 0;
+    int position = 0;
 
     for (space; space > 0; space--) {
-        i = space - rank + nbSpacesMAX(size);
+        i = space - rank + nbSpaces_max(size) - position;
         my_put_space(i);
+        position = 0;
+        if (rank <= *rank_sup) {
+            nbr_star += 4;
+            position += 2;
+        }
+        if (rank == 0)
+            nbr_star += 4;
         my_put_star(nbr_star);
-        nbr_star += 4;
         my_putchar('\n');
     }
-}
+}// HEREEEEEEEE
 
 int leaves(int size)
 {
-    for (int rank = 0; rank < size; rank++)
-        triangle(rank, size);
+    int rank_sup = 0;
+    for (int rank = 0; rank < size; rank++) {
+        rank_sup++;
+        triangle(rank, size, &rank_sup);
+    }
 }
 
 void tree(int size)
