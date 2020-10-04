17a733bee0558a720cfde7c2d5af79dc108ef2e8
pierre1754 <pierre1754@outlook.com>
2020-10-04 22:16:56 +0200

    aled

--- tree.c
+++ tree.c
@@ -39,7 +39,10 @@ int heigh(int size)
 
 int nbspaces_max(int size)
 {
-    return (4 * size) - size + size / 2;
+    int res = size * 3;
+    if (size >= 3)
+        res += size - 2;
+    return res;
 }
 
 void my_put_star(int nbr_star)
@@ -55,23 +58,6 @@ void my_put_space(int space, int rank, int size)
     }
 }
 
-/* void my_correction(int rank, int size, int *correction, int *star_in_more)
-{
-    int space = 4 + rank;
-    int d = 0;
-
-    for (; space > 0; space--)
-        d = space - rank + nbspaces_max(size) - *star_in_more / 4;
-    if (d != -1) {
-        while (d + *correction != -1) {
-            if (d < -1)
-                ++*correction;
-            else if (d > -1)
-                --*correction;
-        }
-    }
-} */
-
 void triangle(int part, int size, int *nbr_star, int *d)
 {
     int rank = 4 + part;
@@ -82,6 +68,9 @@ void triangle(int part, int size, int *nbr_star, int *d)
     for (; rank > 0; rank--) {
         space--;
         if (part != 0 && rank == 4 + part) {
+            if (part <= 4) {
+                space++;
+            }
             space -= part + part - 1;
         }
         my_put_space(space, part, size);
@@ -103,18 +92,6 @@ void leaves(int size)
     int part = 0;
     int nbr_star = 1;
 
-    /* for (; rank < size; rank++) {
-        correction = 0;
-        my_correction(rank, size, &correction, &star_in_more);
-        star_in_more += part_section;
-        if (j == 1) {
-            part_section += 4 ;
-            j = 0;
-        }
-        j++;
-    }
-    j = 0;
-    part_section = 8; */
     for (part = 0; part < size; part++) {
         triangle(part, size, &nbr_star, &d);
         nbr_star -= 4;
