adbd5a014b2e9a0026edc7cc4a064798490cb38a
pierre1754 <pierre1754@outlook.com>
2020-10-04 21:35:46 +0200

    argh

--- tree.c
+++ tree.c
@@ -5,6 +5,8 @@
 ** 1/10/2020
 */
 
+#include <stdio.h>
+
 void my_putchar(char c);
 
 void trunk(int size)
@@ -15,10 +17,10 @@ void trunk(int size)
 
     if (size % 2 == 0)
         h--;
-    for (y; y < size; y++) {
+    for (; y < size; y++) {
         for (int g = 0; g < size * 3 + h; g++)
             my_putchar(' ');
-        for (x; x < size - h; x++)
+        for (; x < size - h; x++)
             my_putchar('|');
         my_putchar('\n');
         x = 0;
@@ -28,23 +30,16 @@ void trunk(int size)
 int heigh(int size)
 {
     int result = 0;
-    int cpSizes = 0;
+    int cp_sizes = 0;
 
-    for (cpSizes; cpSizes != size; cpSizes++)
-        result = result + 4 + cpSizes;
+    for (; cp_sizes != size; cp_sizes++)
+        result = result + 4 + cp_sizes;
     return result;
 }
 
 int nbspaces_max(int size)
 {
-    int result = (4 * size) - size + size / 2;
-    return result;
-}
-
-void my_put_space(int space_var)
-{
-    for (int k = 0; k <= space_var; k++)
-        my_putchar(' ');
+    return (4 * size) - size + size / 2;
 }
 
 void my_put_star(int nbr_star)
@@ -53,12 +48,19 @@ void my_put_star(int nbr_star)
         my_putchar('*');
 }
 
-void my_correction(int rank, int size, int *correction, int *star_in_more)
+void my_put_space(int space, int rank, int size)
+{
+    for (int k = 0; k <= space - rank; k++) {
+        my_putchar(' ');
+    }
+}
+
+/* void my_correction(int rank, int size, int *correction, int *star_in_more)
 {
     int space = 4 + rank;
     int d = 0;
 
-    for (space; space > 0; space--)
+    for (; space > 0; space--)
         d = space - rank + nbspaces_max(size) - *star_in_more / 4;
     if (d != -1) {
         while (d + *correction != -1) {
@@ -68,34 +70,40 @@ void my_correction(int rank, int size, int *correction, int *star_in_more)
                 --*correction;
         }
     }
-}
+} */
 
-void triangle(int rank, int size, int *star_in_more, int *correction)
+void triangle(int part, int size, int *nbr_star, int *d)
 {
-    int nbr_star = 1 + *star_in_more;
-    int space = 4 + rank;
-    int cp_space = space;
+    int rank = 4 + part;
+    int space = nbspaces_max(size);
     int i = 0;
-    int d = 0;
 
-    for (space; space > 0; space--) {
-        i = space - rank + nbspaces_max(size) - *star_in_more / 4 + *correction;
-        my_put_space(i);
-        my_put_star(nbr_star);
-        my_putchar('\n');
-        nbr_star += 4;
+
+    for (; rank > 0; rank--) {
+        space--;
+        if (part != 0 && rank == 4 + part) {
+            space -= part + part - 1;
         }
+        my_put_space(space, part, size);
+        my_put_star(*nbr_star);
+        my_putchar('\n');
+        *nbr_star += 4;
+    }
 }
 
-int leaves(int size)
+void leaves(int size)
 {
     int star_in_more = 0;
     int part_section = 8;
     int j = 0;
+    int d = 4;
+    int x = 0;
+    int h = 2;
     int correction = 0;
-    int rank = 0;
+    int part = 0;
+    int nbr_star = 1;
 
-    for (rank; rank < size; rank++) {
+    /* for (; rank < size; rank++) {
         correction = 0;
         my_correction(rank, size, &correction, &star_in_more);
         star_in_more += part_section;
@@ -106,16 +114,18 @@ int leaves(int size)
         j++;
     }
     j = 0;
-    star_in_more = 0;
-    part_section = 8;
-    for (rank = 0; rank < size; rank++) {
-        triangle(rank, size, &star_in_more, &correction);
-        star_in_more += part_section;
-        if (j == 1) {
-            part_section += 4 ;
+    part_section = 8; */
+    for (part = 0; part < size; part++) {
+        triangle(part, size, &nbr_star, &d);
+        nbr_star -= 4;
+        if (j == 2) {
+            d += 4;
             j = 0;
         }
+        h++;
         j++;
+        nbr_star = nbr_star - d;
+        /* printf("%d\n", d); */
     }
 }
 
